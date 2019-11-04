06d9fd0648b6c89465d831057473e4571da2da9b
Duy Phung <duypv98@gmail.com>
2019-11-04 22:48:57 +0700

    chore: change DB_URL

--- configs/dev.js
+++ configs/dev.js
@@ -6,8 +6,10 @@ const DB_PWD = process.env.DB_PWD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 
-const DB_URL = `mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/easy-event-v2`;
+const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/easy-event-v2`;
 
 module.exports = {
-    DB_URL
+    DB_URL,
+    DB_USER,
+    DB_PWD
 }
--- configs/local.js
+++ configs/local.js
@@ -1,5 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const DB_USER = process.env.DB_USER;
+const DB_PWD = process.env.DB_PWD;
 const DB_URL = 'mongodb://127.0.0.1:27017/easy-event-v2';
 
+
 module.exports = {
-    DB_URL
+    DB_URL,
+    DB_USER,
+    DB_PWD
 }
