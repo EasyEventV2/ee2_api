211a357d9c7cbf2f9d23f130d288f01545cdd4af
Duy Phung <duypv98@gmail.com>
2019-11-24 08:45:41 +0700

    refactor: solved duplicate imports

---
+++ configs/base.js
@@ -0,0 +1,13 @@
+const {
+  DB_HOST, DB_PORT, DB_USER, DB_PWD, JWT_SECRET_KEY,
+} = process.env;
+
+const baseConfig = {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PWD,
+  JWT_SECRET_KEY,
+}
+
+export default baseConfig;
--- configs/local.js
+++ configs/local.js
@@ -1,11 +1,9 @@
-const { DB_USER, DB_PWD, SECRET_KEY } = process.env;
+import baseConfig from 'configs/base';
 const DB_URL = 'mongodb://127.0.0.1:27017/easy-event-v2';
 
 const localConfig = {
   DB_URL,
-  DB_USER,
-  DB_PWD,
-  SECRET_KEY,
+  ...baseConfig,
 };
 
 export default localConfig;
