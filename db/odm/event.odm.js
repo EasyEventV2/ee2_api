d1406075a2b19f701b83c2aab88bbaaa9962424c
Duy Phung <duypv98@gmail.com>
2019-12-01 23:17:07 +0700

    feat: added selectField constant

--- db/odm/event.odm.js
+++ db/odm/event.odm.js
@@ -2,6 +2,11 @@
 import Event from 'db/models/Event';
 import Group from 'db/models/Group';
 
+const SELECT_FIELD = {
+  YES: 1,
+  NO: 0,
+};
+
 /**
  *
  * @param {String} userId
@@ -9,11 +14,23 @@ import Group from 'db/models/Group';
 async function findByUserId(userId) {
   const listEvents = await Group.find(
     { users: userId },
-    { _id: 0 },
+    { _id: SELECT_FIELD.NO }, // This is Group'id, that will not be returned.
   ).populate('event');
+  return listEvents;
+}
+
+/**
+ *
+ */
+async function findAll() {
+  const currentTime = new Date();
+  const listEvents = await Event.find(
+    { end_time: { $gte: currentTime } },
+  );
   return listEvents;
 }
 
 export default {
   findByUserId,
+  findAll,
 };
