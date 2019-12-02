d931e2c0d8aab227e290c3845f4fe67898b51f4d
Duy Phung <duypv98@gmail.com>
2019-12-02 23:22:13 +0700

    feat: added paginate error

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,6 +1,18 @@
+import { PageNotFoundError } from 'common/error';
+
+/**
+ *
+ * @param {Number} totalItems
+ * @param {Number} itemsPerPage
+ * @param {Object} page
+ */
 function getPaginatedObject(totalItems, itemsPerPage, page) {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   const currentPage = (!page) ? 1 : parseInt(page, 10);
+  if (!currentPage // parsing Error
+    || (currentPage && (currentPage < 0 || currentPage > totalPages))) {
+    throw new PageNotFoundError();
+  }
   const offset = (currentPage - 1) * itemsPerPage;
   const limit = itemsPerPage;
   return {
