a938adc8cd14b08743f2b2a3bf35b30d5bea2d8d
Duy Phung <duypv98@gmail.com>
2019-12-22 01:17:28 +0700

    chore: removed comments

--- services/mailgun.js
+++ services/mailgun.js
@@ -5,16 +5,6 @@ const { MAILGUN_API_KEY, DOMAIN } = configs;
 
 const mailgun = Mailgun({ apiKey: MAILGUN_API_KEY, domain: DOMAIN });
 
-// function send(data) {
-//   const report = mailgun.messages().send(data);
-//   return report;
-// }
-
-// export default {
-//   default: mailgun,
-//   send,
-// };
-
 class _MailgunService {
   constructor() {
     this.api = mailgun;
