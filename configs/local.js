a13135bf7ba7e812690d8b038dffdce010939944
Duy Phung <duypv98@gmail.com>
2019-12-27 00:39:31 +0700

    feat: added FE_URL

--- configs/local.js
+++ configs/local.js
@@ -1,7 +1,9 @@
 const DB_URL = 'mongodb://127.0.0.1:27017/easy-event-v2';
+const FE_URL = 'http://127.0.0.1:3000/';
 
 const localConfig = {
   DB_URL,
+  FE_URL,
 };
 
 export default localConfig;
--- configs/prod.js
+++ configs/prod.js
@@ -1,9 +1,11 @@
 import baseConfig from 'configs/base';
 
 const DB_URL = `mongodb://${baseConfig.DB_HOST}:${baseConfig.DB_PORT}/easy-event-v2`;
+const FE_URL = 'https://easy-event.tk/';
 
 const prodConfig = {
   DB_URL,
+  FE_URL,
 };
 
 export default prodConfig;
