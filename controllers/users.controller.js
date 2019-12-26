7a32e97e8ab7d658de30835a436504293227f42b
Duy Phung <duypv98@gmail.com>
2019-12-26 21:10:24 +0700

    fix: removed unnecessary params

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,8 +2,7 @@ import asyncDec from 'utils/asyncDecoration';
 import userCore from 'core/users.core';
 
 const getUserById = asyncDec(async (req, res) => {
-  const userId = (req.params.userId === 'me') ? req.uid : req.params.userId;
-  const dataResponse = await userCore.findUserById(userId);
+  const dataResponse = await userCore.findUserById(req.uid);
   res.json({
     data: dataResponse,
   });
